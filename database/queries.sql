2da7d74610a2b656965dc5b078d7db7b07ce505e
wiki05ww <wiki05.ww@gmail.com>
2026-04-30 19:38:56 +0200

    Update queries.sql

--- database/queries.sql
+++ database/queries.sql
@@ -1,1 +1,15 @@
+select u.name, m.title, r.rental_date
+from rentals r
+join users u on r.user_id = u.user_id
+join movies m on r.movie_id = m.movie_id
 
+select m.title, count(*) as rentals_count
+from rentals r
+join movies m on r.movie_id = m.movie_id
+group by m.title
+order by rentals_count desc
+
+select m.title, avg(r.rating) as avg_rating
+from ratings r
+join movies m on r.movie_id = m.movie_id
+group by m.title
